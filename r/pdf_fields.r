5feb29954b393dd09bd5700439daddfbb127ce37
Russ Jones <your.email@work.com>
2026-08-14 20:19:32 +0100

    Changed the script folder name from 'r' to 'R', Began script to hold project specific functions such as setting each field value to its name or setting a value for a specific field.

--- r/pdf_fields.r
+++ r/pdf_fields.r
@@ -2,9 +2,11 @@
 # Script: pdf_fields.r
 # Description: 
 #   Reads the fields from a given pdf file
+#   Sets the fields from an export form EpiTrax
 #
-#
-# Steps:
+#    Note that the following fields are malformed in the pdf template, specifically the factor levels:
+#    'Exposure'
+#    'Timeframe for Tdap'
 #
 # -------------------------------------------------------------------------------------->
 # Author: Russ Jones
@@ -13,6 +15,7 @@
 # -------------------------------------------------------------------------------------->
 library(purrr)
 library(staplr)
+source("R/field_functions.r")  # load custom functions for this project.
 
 file <- "forms/Pertussis-Fillable-Feb2022.pdf"
 fields <- get_fields(input_filepath = file)
@@ -20,29 +23,23 @@ map(fields, ~ .x$type) |>
   unlist() |> 
   table()
 
-# set the value of the list item to a value to identify the variable name, or button to check
-set_text_value <- function(item){
-  type <- item[["type"]]
-  
-  item[["value"]] <- switch(
-    EXPR = type,
-     "Button" = factor(
-       levels(item[["value"]])[2],
-       levels = levels(item[["value"]])
-     ),
-    "Text"   = item[["name"]]
-  )
-  return(item)
-}
 
+# avoids the problematic fields.  Change this to all fields when pdf form is corrected. 
 fields_value_set <- map(fields[c(1:376, 379:384)], set_text_value)
-fields_value_set <- map(fields[c(1:384)], set_text_value)
+#fields_value_set <- map(fields[c(1:384)], set_text_value)
 
+  
+
+fields_value_set$`Timeframe for Tdap` <- set_specific(item = fields_value_set$`Timeframe for Tdap`,
+                                                     value = "at delivery")
+fields_value_set$Exposure <- set_specific(fields_value_set$Exposure, "close contact")
+fields_value_set$`Timeframe for Tdap` <- NULL
+fields_value_set$Exposure <- NULL
 out_file <- "Pertussis_test.pdf"
 set_fields(input_filepath = file, output_filepath = out_file, fields = fields_value_set, overwrite = TRUE)
 
-problem_fields <- fields[377:378]
-
+problem_fields <- fields_value_set[377:378]
+problem_fields
 
 
 
@@ -60,10 +57,5 @@ fields_value_set[["SEX"]]$value <- factor(
 
 buttons <- keep(.x = fields_value_set, .p = \(x) x[["type"]] == "Button") 
 map(buttons, ~.x[["value"]])
-
-
-
-
-
 
 
